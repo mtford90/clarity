35bd712983d0632fe4c9e78d756ecc2eb02246e3
Michael Ford <mtford@gmail.com>
2014-02-01 13:43:06 +0000

    Get + set

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
  * Module dependencies.
  */
 
+var Logger = require('./config').logger;
+
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
@@ -16,11 +18,14 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
+//app.use(express.favicon());
 app.use(express.logger('dev'));
+
+// Middleware stack
+//app.use(express.bodyParser());
+app.use(express.methodOverride());
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -29,31 +34,59 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.sshPool = ssh.SSHConnection({
-    host: '46.51.201.85',
-    port: 22,
-    username: 'ubuntu',
-    privateKey: require('fs').readFileSync('/Users/mtford/Dropbox/Drake/Server-Side/dev.pem')
-});
+var Data = require('./lib/data');
 
-app.get('/', routes.index);
+Data.sequelize.sync().success(function() {
 
-app.get('/users', user.list);
+    app.defaultUser = Data.User.create({
+        user: 'default'
+    }).success(function (user) {
+           Logger.info('Created default user', user);
+        });
 
-app.get('/stats/swap', function (req, res) {
-    ssh.swapUsedPercentage(function (error, perc) {
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ a: perc }));
+    app.sshPool = ssh.SSHConnection({
+        host: '46.51.201.85',
+        port: 22,
+        username: 'ubuntu',
+        privateKey: require('fs').readFileSync('/Users/mtford/Dropbox/Drake/Server-Side/dev.pem')
     });
-});
 
-app.get('/stats/cpu', function (req, res) {
-    ssh.averageLoad(function (error, avg) {
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(avg));
+    app.get('/', routes.index);
+
+    app.get('/users', user.list);
+
+    app.get('/stats/swap', function (req, res) {
+        ssh.swapUsedPercentage(function (error, perc) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({ a: perc }));
+        });
     });
+
+    app.get('/stats/cpu', function (req, res) {
+        ssh.averageLoad(function (error, avg) {
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify(avg));
+        });
+    });
+
+    app.get('/users/default/grid', function (req, res) {
+        res.setHeader('Content-Type', 'application/json');
+        var grid = app.defaultUser.grid;
+        Logger.debug('Grid: ', grid);
+        res.end(grid ? grid.toString() : "");
+    });
+
+    app.put('/users/default/grid', function (req, res, next) {
+        var grid = JSON.stringify(req.body);
+        Logger.debug('raw body is ', grid);
+        app.defaultUser.grid = grid;
+        res.end();
+    });
+
+    http.createServer(app).listen(app.get('port'), function(){
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+
 });
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
+
